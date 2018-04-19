dc679948869fcb806ad39f153422336799b516b5
omeee <>
2018-04-18 22:40:32 -0400

    "regler probleme de connection sql dans sqlConnection"

--- src/main/java/com/inf600c/dossiermedical/fondation/SqlConnection.java
+++ src/main/java/com/inf600c/dossiermedical/fondation/SqlConnection.java
@@ -16,18 +16,27 @@ import java.util.logging.Logger;
  * @author Lado
  */
 public class SqlConnection {
-    
-    String path = "jdbc:sqlite:C:/Users/Lado/Desktop/Docs/Cours/INF5153/DossierMedical/DossierDataBase.db";
+
+    String url = "DossierDataBase.db";
+    String path = "jdbc:sqlite:" + url;
     Connection conn = null;
-    
-    public Connection getConnection() throws SQLException{
-        
+
+    public Connection getConnection() throws SQLException {
+        try {
+            Class.forName("org.sqlite.JDBC").newInstance();
+        } catch (Exception ex) {
+            System.out.println("Error Connecting.");
+        }
+        try {
             conn = DriverManager.getConnection(path);
             System.out.println("Connection to SQLite a été établie.");
+        } catch (SQLException ex) {
+            System.out.println("SQLException: " + ex.getMessage());
+            System.out.println("SQLState: " + ex.getSQLState());
+            System.out.println("VendorError: " + ex.getErrorCode());
+        }
 
-        
-       return conn; 
+        return conn;
     }
-    
-    
+
 }
