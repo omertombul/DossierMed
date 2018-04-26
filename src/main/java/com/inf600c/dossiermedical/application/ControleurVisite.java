96a5656914a0e45afb0c25b7a3f19f5f6ec79768
LADO\Lado <lado.k252@gmail.com>
2018-04-25 23:46:43 -0400

    Ajout de classe ControleurVisite.

---
+++ src/main/java/com/inf600c/dossiermedical/application/ControleurVisite.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.inf600c.dossiermedical.application;
+
+import com.inf600c.dossiermedical.domaine.Medecin;
+import com.inf600c.dossiermedical.domaine.Visite;
+import com.inf600c.dossiermedical.servicestechniques.DB;
+import com.inf600c.dossiermedical.servicestechniques.DateVisite;
+import java.sql.SQLException;
+
+
+/**
+ *
+ * @author Lado
+ */
+public class ControleurVisite {
+    DB db = new DB();
+    Visite visite = new Visite();
+    
+    public void ajouterMedecinDansVisite(int codeEmploye){
+        //String specialite = db.getspecialteMedecin(codeEmploye);
+    //    visite.setMedecin(new Medecin(codeEmploye, specialite));
+    }
+    
+    
+    public void sauvegarderVisite() throws SQLException{
+        
+        int idVisite = db.getLastId("Visite", "idVisite") + 1;
+        
+        visite.setIdVisite(idVisite);
+        visite.setDateVisite(DateVisite.dateDAujourdhui());
+//        visite.setMedecin(medecin);
+        
+        
+        db.sauvegarderVisite(visite);
+    }
+
+    public void ajouterNote(String note){
+        visite.setNote(note);
+    }
+    
+    public void ajouterEtablissement(String nomEtablissement){
+        visite.setNomEtablissement(nomEtablissement);
+    }
+    
+}
